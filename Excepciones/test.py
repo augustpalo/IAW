bab195a530d5e87ec72872cdb6b80d5d4019df5a
CJ\apal <august.horacio.paloluoma@iesciudadjardin.com>
2023-10-24 12:51:01 +0200

    excepciones

---
+++ Excepciones/test.py
@@ -0,0 +1,16 @@
+""" try:
+    #Codigo a ejecutar pero podria haber errores en este bloque
+except <error>:
+
+else:
+    #Esto se ejecuta si el bloque se ejectua con errores
+
+finally:
+
+ """
+def mas_10(num):
+    return num + 10;
+
+numero = int(input("Inserte un numero que quieras sumarle 10"))
+
+print(mas_10(numero))
